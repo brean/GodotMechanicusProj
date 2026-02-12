dc5e539ffb36fd8c94aca981662ee9600fc7bf6b
Andreas Bresser <self@andreasbresser.de>
2026-02-12 22:20:38 +0100

    update chain to use Generic6DOFJoint instead of the Jolt exclusive one

--- Prefabs/Chain1/jolt_generic_6dof_joint_3d_4.gd
+++ Prefabs/Chain1/jolt_generic_6dof_joint_3d_4.gd
@@ -1,9 +1,9 @@
-extends JoltGeneric6DOFJoint3D
+extends Generic6DOFJoint3D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("correct:",self.get_param_y(JoltGeneric6DOFJoint3D.PARAM_ANGULAR_LIMIT_LOWER))
+	print("correct:", self.get_param_y(Generic6DOFJoint3D.PARAM_ANGULAR_LOWER_LIMIT))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
